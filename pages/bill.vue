3ecd0d63cad7b4990a6b8e108593959b8489fbc3
TrongQuan <142089306+DoanTrongQuan@users.noreply.github.com>
2024-05-17 09:58:50 +0700

    .

--- pages/bill.vue
+++ pages/bill.vue
@@ -1,58 +1,87 @@
 <template>
-  <UDashboardPage>
-    <UDashboardPanel grow>
-      <UDashboardNavbar
-        title="Quản lí hóa đơn"
-        :badge="bills.length"
-      >
-      </UDashboardNavbar>
-    <div class="overflow-y-auto">
-    <v-card flat>
-      <v-card-title class="d-flex align-center pe-2">
-        <v-spacer></v-spacer>
-
-        <v-text-field
-          density="compact"
-          label="Tìm kiếm hóa đơn"
-          v-model="search"
-          prepend-inner-icon="mdi-magnify"
-          variant="solo-filled"
-          flat
-          hide-details
-          single-line
-        ></v-text-field>
-        <div class = "mx-5">
-        <!-- <UButton @click="">Thêm đồ ăn</UButton> -->
-      </div>
-      </v-card-title>
-
-      <v-divider></v-divider>
-      <v-data-table
-        :items-per-page-options="[
-          { value: 5, title: '05' },
-          { value: 10, title: '10' },
-          { value: 15, title: '15' },
-          { value: 20, title: '20' },
-        ]"
-        v-model:search="search"
-        :items="bills"
-        hover
-        :headers="headersBill"
-      >
-        <template v-slot:item.status="{ item }">
-          <div class="text-end">
-            <v-chip
-              :color="item.status === 2 ? 'green' : 'red'"
-              :text="item.status === 2 ? 'Đã thanh toán' :'Chưa thanh toán' "
-              class="text-uppercase"
-              size="small"
-              label
-            ></v-chip>
+    <UDashboardPage>
+      <UModal v-model="isShowDeleteBill" class="w-full" prevent-close>
+        <div class="py-1 px-4">
+          <div class="py-2 flex justify-end">
+            <UButton
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="isShowDeleteBill = false"
+            />
           </div>
-        </template>
-      </v-data-table>
-    </v-card>
+          <div class="py-3 px-3">
+            <h1 class="text-2xl font-sans text-center font-semibold pb-3">
+              Xóa hóa đơn theo thời gian
+            </h1>
+            <UForm :state="deleteBill" class="space-y-6" @submit="onSubmit">
+              <div class="grid grid-cols-4 gap-2 mt-5">
+                <p class="col-span-1">Bắt đầu</p>
+                <input class="col-span-2" v-model = "deleteBill.start" type="datetime-local">
+              </div>
+              <div class="grid grid-cols-4 gap-2 mt-5">
+                <p class="col-span-1">Kết thúc</p>
+                <input class="col-span-2" v-model = "deleteBill.end" type="datetime-local">
+              </div>
+              <div class="w-full flex justify-center">
+                <UButton type = "submit">Xác nhận</UButton> 
+              </div>
+            </UForm>
+          </div>
+        </div>
+      </UModal>
+<UDashboardPanel grow>
+  <UDashboardNavbar title="Quản lí rạp chiếu phim"  class="text-2xl"/>
+
+<div class="overflow-y-auto">
+  <v-card>
+  <v-card-title class="d-flex align-center pe-2">
+    <v-spacer></v-spacer>
+
+    <v-text-field
+      density="compact"
+      label="Tìm kiếm hóa đơn"
+      v-model="search"
+      prepend-inner-icon="mdi-magnify"
+      variant="solo-filled"
+      flat
+      hide-details
+      single-line
+    ></v-text-field>
+    <div class = "mx-5">
+    <UButton @click="removeAll">Dọn dẹp</UButton>
   </div>
+  </v-card-title>
+
+  <v-divider></v-divider>
+  <v-data-table
+    :items-per-page-options="[
+      { value: 5, title: '05' },
+      { value: 10, title: '10' },
+      { value: 15, title: '15' },
+      { value: 20, title: '20' },
+    ]"
+    v-model:search="search"
+    :items="bills"
+    hover
+    :headers="headersBill"
+  >
+    <template v-slot:item.status="{ item }">
+      <div class="text-end">
+        <v-chip
+          :color="item.status === 2 ? 'green' : 'red'"
+          :text="item.status === 2 ? 'Đã thanh toán' :'Chưa thanh toán' "
+          class="text-uppercase"
+          size="small"
+          label
+        ></v-chip>
+      </div>
+    </template>
+  </v-data-table>
+</v-card>
+</div>
+  
 </UDashboardPanel>
 </UDashboardPage>
 </template>
@@ -67,6 +96,11 @@ const bills = computed(() => {
     return billStore.bills;
 })
 const search = ref('')
+const isShowDeleteBill = ref(false)
+const deleteBill = ref({
+  start:'',
+  end:''
+})
 
 billStore.getAllBill()
 
@@ -84,8 +118,15 @@ const headersBill = ref([
   { title: "Ngày tạo", key: "createTime", align: "center" },
   { title: "Trạng thái", key: "status", align: "center" },
 ]);
+
+const removeAll  = () =>{
+  isShowDeleteBill.value = true;
+}
 </script> 
 
 <style>
 
 </style>
+
+
+  
